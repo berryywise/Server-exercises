196bb2fe3aaf6908b48949ad8583114b633341e1
pc <100073317+berryywise@users.noreply.github.com>
2024-01-12 21:30:15 +0100

    Added event handlers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 ////IMPORT MODULES
-
 let http = require('http');
 let fs = require("fs")
+const EventEmitter = require('events');
+
+let eventemitter = new EventEmitter();
+
+eventemitter.on("called", message => console.log(`A ${message} request has been made!`))
+
 
 // STATIC 404 PAGE ERROR
-
 const NOT_FOUND_PATH = "./404.html"
 
 //CREATE SERVER
@@ -16,21 +20,18 @@ let parse = req.url == "/" ? "/index.html" : req.url;
 //READ .HTML FILES IN ROOT DIRECTORY
 
   fs.readFile(`.${parse}`, (err, data) => {
-
     //IF ERROR RESPOND WITH 404 FAULTY PAGE
-
       if(err) {
         res.writeHead(404, {'Content-Type': 'text/html'})
         res.write(fs.readFileSync(NOT_FOUND_PATH));
+        eventemitter.emit("called", "failed")
         return res.end()
       }
-
       //IF NO ERROR RESPOND WITH 200 OK AND REQUESTED DATA
-
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.write(data);
+      eventemitter.emit("called", "succesfull")
       return res.end();
-
   })
 
   //ENABLE URL MODULE FOR CODE DOWN HERE
